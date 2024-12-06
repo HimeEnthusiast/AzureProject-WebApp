beafc8774a5298619b99649ed0154a08a2f0beb3
HimeEnthusiast <piantonibella@gmail.com>
2024-12-06 03:16:54 +0000

    Updated default port for web apps

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 
 const app = express()
 
-app.listen(3000, () => {
+app.listen(80, () => {
     console.log("Server running on port 3000")
 })
 app.set('view engine', 'ejs')
